71ed695067efa1c3400b663535cea537d1151cbf
luo_hao_yu <2523273301@qq.com>
2021-10-23 16:10:27 +0800

    登陆验证方法

--- src/main/java/com/example/autumn_valley_agriculture/config/CorsConfig.java
+++ src/main/java/com/example/autumn_valley_agriculture/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	//解决跨域问题
 
 	private CorsConfiguration buildConfig() {
-		System.out.println(111);
+		System.out.println();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.addAllowedOrigin("*");
 		corsConfiguration.addAllowedHeader("*");
